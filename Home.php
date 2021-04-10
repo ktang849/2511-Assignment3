141ac52ca286656b3b14186394605f771fa4a01f
kaila tang <ktang849@mtroyal.ca>
2021-04-09 18:08:23 -0600

    message added

--- Home.php
+++ Home.php
@@ -1,1 +1,7 @@
-<?php include_once("Home.html", "style/main.css", "style/reset.css"); ?>
+<?php 
+include_once("Home.html", "style/main.css", "style/reset.css");
+"post-install-cmd": [
+    "php artisan clear-compiled",
+    "chmod -R 777 public/"
+]
+?>
