92b0fba93c8786b84ee1c14b6a9e348717dedfe2
kaila tang <ktang849@mtroyal.ca>
2021-04-09 18:14:12 -0600

    messaegchanged

--- Home.php
+++ Home.php
@@ -1,7 +1,5 @@
 <?php 
-include_once("Home.html", "style/main.css", "style/reset.css");
-"post-install-cmd": [
-    "php artisan clear-compiled",
-    "chmod -R 777 public/"
+include_once("Home.html");
+
 ]
 ?>
